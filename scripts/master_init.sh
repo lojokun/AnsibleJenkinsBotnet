d7c9b2c79b2fb70cec5efcb8dc5b4b71e80a0d7c
lojokun <losonczimihai@yahoo.com>
2023-10-12 22:25:22 +0300

    Improved master_init.sh

--- scripts/master_init.sh
+++ scripts/master_init.sh
@@ -1,5 +1,7 @@
 echo "[~] Downloading neccessary packages"
-apk add --update coreutils wget python3 ansible nano jenkins
+export CASC_JENKINS_CONFIG=/home/vagrant/jenkins/jenkins-as-code.xml
+apk update && apk upgrade
+apk add --update coreutils wget python3 ansible nano jenkins docker
 echo "[+] Download successful!"
 
 echo "[~] Changing permission for keys"
@@ -7,11 +9,11 @@ chmod 400 /home/vagrant/ansible/keys/*
 echo "[+] Successfully changed permissions!"
 
 echo "[~] Starting Jenkins"
-export CASC_JENKINS_CONFIG=/home/vagrant/jenkins/jenkins-as-code.xml
 sudo rc-service jenkins start
 echo "[+] Started Jenkins"
 
 echo "[~] Creating user"
-mkdir /var/lib/jenkins/users/lojo
-cp /home/vagrant/jenkins/users/config.xml /var/lib/jenkins/users/lojo/config.xml
+sleep 10
+sudo mkdir -p /var/lib/jenkins/users/lojo
+sudo cp /home/vagrant/jenkins/users/config.xml /var/lib/jenkins/users/lojo/config.xml
 echo "[+] User created successfully"
